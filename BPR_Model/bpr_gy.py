f0256a8c6792d1d4f54886a15695145803352202
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-04-20 00:25:24 +0900

    feat : new bpr 코드 추가

--- BPR_Model/bpr_gy.py
+++ BPR_Model/bpr_gy.py
@@ -24,8 +24,8 @@ path = './dataset/'
 def load_all(test_num=100):
 	""" We load all the three file here to save time in each epoch. """
 	train_data = pd.read_csv(
-		path + 'history_train.csv', 
-		sep='\t', header=None, names=['user', 'business'], 
+		path + 'train.csv', 
+		sep=',', header=None, names=['user', 'business'], 
 		usecols=[0, 1], dtype={0: np.int32, 1: np.int32})
 
 	user_num = train_data['user'].max() + 1
@@ -39,7 +39,7 @@ def load_all(test_num=100):
 		train_mat[x[0], x[1]] = 1.0
 
 	test_data = []
-	with open(path + 'history_test_negative.csv', 'r') as fd:
+	with open(path + 'test_neg.csv', 'r') as fd:
 		line = fd.readline()
 		while line != None and line != '':
 			arr = line.split('\t')
@@ -80,7 +80,7 @@ class BPRData(data.Dataset):
 		return self.num_ng * len(self.features) if \
 				self.is_training else len(self.features)
 
-	def __getbusiness__(self, idx):
+	def __getitem__(self, idx):
 		features = self.features_fill if \
 				self.is_training else self.features
 
@@ -152,107 +152,108 @@ class BPR(nn.Module):
 
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--lr", 
-	type=float, 
-	default=0.01, 
-	help="learning rate")
-parser.add_argument("--lamda", 
-	type=float, 
-	default=0.001, 
-	help="model regularization rate")
-parser.add_argument("--batch_size", 
-	type=int, 
-	default=4096, 
-	help="batch size for training")
-parser.add_argument("--epochs", 
-	type=int,
-	default=50,  
-	help="training epoches")
-parser.add_argument("--top_k", 
-	type=int, 
-	default=10, 
-	help="compute metrics@top_k")
-parser.add_argument("--factor_num", 
-	type=int,
-	default=32, 
-	help="predictive factors numbers in the model")
-parser.add_argument("--num_ng", 
-	type=int,
-	default=4, 
-	help="sample negative businesses for training")
-parser.add_argument("--test_num_ng", 
-	type=int,
-	default=99, 
-	help="sample part of negative businesses for testing")
-parser.add_argument("--out", 
-	default=True,
-	help="save model or not")
-parser.add_argument("--gpu", 
-	type=str,
-	default="0",  
-	help="gpu card ID")
-args = parser.parse_args()
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--lr", 
+		type=float, 
+		default=0.01, 
+		help="learning rate")
+	parser.add_argument("--lamda", 
+		type=float, 
+		default=0.001, 
+		help="model regularization rate")
+	parser.add_argument("--batch_size", 
+		type=int, 
+		default=4096, 
+		help="batch size for training")
+	parser.add_argument("--epochs", 
+		type=int,
+		default=50,  
+		help="training epoches")
+	parser.add_argument("--top_k", 
+		type=int, 
+		default=10, 
+		help="compute metrics@top_k")
+	parser.add_argument("--factor_num", 
+		type=int,
+		default=32, 
+		help="predictive factors numbers in the model")
+	parser.add_argument("--num_ng", 
+		type=int,
+		default=4, 
+		help="sample negative businesses for training")
+	parser.add_argument("--test_num_ng", 
+		type=int,
+		default=99, 
+		help="sample part of negative businesses for testing")
+	parser.add_argument("--out", 
+		default=True,
+		help="save model or not")
+	parser.add_argument("--gpu", 
+		type=str,
+		default="0",  
+		help="gpu card ID")
+	args = parser.parse_args()
 
-os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
-cudnn.benchmark = True
+	os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+	cudnn.benchmark = True
 
 
-############################## PREPARE DATASET ##########################
-train_data, test_data, user_num ,business_num, train_mat = load_all()
+	############################## PREPARE DATASET ##########################
+	train_data, test_data, user_num ,business_num, train_mat = load_all()
 
-# construct the train and test datasets
-train_dataset = BPRData(
-		train_data, business_num, train_mat, args.num_ng, True)
-test_dataset = BPRData(
-		test_data, business_num, train_mat, 0, False)
-train_loader = data.DataLoader(train_dataset,
-		batch_size=args.batch_size, shuffle=True, num_workers=4)
-test_loader = data.DataLoader(test_dataset,
-		batch_size=args.test_num_ng+1, shuffle=False, num_workers=0)
+	# construct the train and test datasets
+	train_dataset = BPRData(
+			train_data, business_num, train_mat, args.num_ng, True)
+	test_dataset = BPRData(
+			test_data, business_num, train_mat, 0, False)
+	train_loader = data.DataLoader(train_dataset,
+			batch_size=args.batch_size, shuffle=True, num_workers=4)
+	test_loader = data.DataLoader(test_dataset,
+			batch_size=args.test_num_ng+1, shuffle=False, num_workers=0)
 
-########################### CREATE MODEL #################################
-model = BPR(user_num, business_num, args.factor_num)
-model.cuda()
+	########################### CREATE MODEL #################################
+	model = BPR(user_num, business_num, args.factor_num)
+	model.cuda()
 
-optimizer = optim.SGD(
-			model.parameters(), lr=args.lr, weight_decay=args.lamda)
-# writer = SummaryWriter() # for visualization
+	optimizer = optim.SGD(
+				model.parameters(), lr=args.lr, weight_decay=args.lamda)
+	# writer = SummaryWriter() # for visualization
 
-########################### TRAINING #####################################
-count, best_hr = 0, 0
-for epoch in range(args.epochs):
-	model.train() 
-	start_time = time.time()
-	train_loader.dataset.ng_sample()
+	########################### TRAINING #####################################
+	count, best_hr = 0, 0
+	for epoch in range(args.epochs):
+		model.train() 
+		start_time = time.time()
+		train_loader.dataset.ng_sample()
 
-	for user, business_i, business_j in train_loader:
-		user = user.cuda()
-		business_i = business_i.cuda()
-		business_j = business_j.cuda()
+		for user, business_i, business_j in train_loader:
+			user = user.cuda()
+			business_i = business_i.cuda()
+			business_j = business_j.cuda()
 
-		model.zero_grad()
-		prediction_i, prediction_j = model(user, business_i, business_j)
-		loss = - (prediction_i - prediction_j).sigmoid().log().sum()
-		loss.backward()
-		optimizer.step()
-		# writer.add_scalar('data/loss', loss.business(), count)
-		count += 1
+			model.zero_grad()
+			prediction_i, prediction_j = model(user, business_i, business_j)
+			loss = - (prediction_i - prediction_j).sigmoid().log().sum()
+			loss.backward()
+			optimizer.step()
+			# writer.add_scalar('data/loss', loss.business(), count)
+			count += 1
 
-	model.eval()
-	HR, NDCG = metrics(model, test_loader, args.top_k)
+		model.eval()
+		HR, NDCG = metrics(model, test_loader, args.top_k)
 
-	elapsed_time = time.time() - start_time
-	print("The time elapse of epoch {:03d}".format(epoch) + " is: " + 
-			time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
-	print("HR: {:.3f}\tNDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
+		elapsed_time = time.time() - start_time
+		print("The time elapse of epoch {:03d}".format(epoch) + " is: " + 
+				time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
+		print("HR: {:.3f}\tNDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
 
-	if HR > best_hr:
-		best_hr, best_ndcg, best_epoch = HR, NDCG, epoch
-		if args.out:
-			if not os.path.exists(path + model_path):
-				os.mkdir(path + model_path)
-			torch.save(model, '{}BPR.pt'.format(path + model_path))
+		# if HR > best_hr:
+		# 	best_hr, best_ndcg, best_epoch = HR, NDCG, epoch
+		# 	if args.out:
+		# 		if not os.path.exists(path + model_path):
+		# 			os.mkdir(path + model_path)
+		# 		torch.save(model, '{}BPR.pt'.format(path + model_path))
 
-print("End. Best epoch {:03d}: HR = {:.3f}, \
-	NDCG = {:.3f}".format(best_epoch, best_hr, best_ndcg))
+	# print("End. Best epoch {:03d}: HR = {:.3f}, \
+	# 	NDCG = {:.3f}".format(best_epoch, best_hr, best_ndcg))
