9e1ec98fbd2e16e93015f88be7892ea67093d187
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-04-21 18:52:44 +0900

    feat : yelp 데이터셋 테스트 코드 추가

--- BPR_Model/bpr_gy.py
+++ BPR_Model/bpr_gy.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd 
 import scipy.sparse as sp
 
+import config
+
 #import path + 
 import os
 import time
@@ -22,11 +24,40 @@ import torch.backends.cudnn as cudnn
 
 path = './dataset/'
 
+# def load_all(test_num=100):
+# 	""" We load all the three file here to save time in each epoch. """
+# 	train_data = pd.read_csv(
+# 		config.train_rating, 
+# 		sep='\t', header=None, names=['user', 'item'], 
+# 		usecols=[0, 1], dtype={0: np.int32, 1: np.int32})
+
+# 	user_num = train_data['user'].max() + 1
+# 	item_num = train_data['item'].max() + 1
+
+# 	train_data = train_data.values.tolist()
+
+# 	# load ratings as a dok matrix
+# 	train_mat = sp.dok_matrix((user_num, item_num), dtype=np.float32)
+# 	for x in train_data:
+# 		train_mat[x[0], x[1]] = 1.0
+
+# 	test_data = []
+# 	with open(config.test_negative, 'r') as fd:
+# 		line = fd.readline()
+# 		while line != None and line != '':
+# 			arr = line.split('\t')
+# 			u = eval(arr[0])[0]
+# 			test_data.append([u, eval(arr[0])[1]])
+# 			for i in arr[1:]:
+# 				test_data.append([u, int(i)])
+# 			line = fd.readline()
+# 	return train_data, test_data, user_num, item_num, train_mat
+
 
 def load_all(test_num=100):
 	""" We load all the three file here to save time in each epoch. """
 	train_data = pd.read_csv(
-		path + 'pinterest-20.train.rating', 
+		path + 'train.csv', 
 		sep=',', header=None, names=['user', 'item'], 
 		usecols=[0, 1], dtype={0: np.int32, 1: np.int32})
 
@@ -41,7 +72,7 @@ def load_all(test_num=100):
 		train_mat[x[0], x[1]] = 1.0
 
 	test_data = []
-	with open(path + 'pinterest-20.test.negative', 'r') as fd:
+	with open(path + 'test_neg_100.csv', 'r') as fd:
 		line = fd.readline()
 		while line != None and line != '':
 			arr = line.split('\t')
