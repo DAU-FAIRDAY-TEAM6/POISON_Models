b5778f991a9d823b4e137198cf57c59438523dea
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-05-20 17:05:54 +0900

    feat : BPR 코드 수정
    모델 평가 부분 코드 사라져서 다시 올립니다.

--- BPR_Model/BPR.py
+++ BPR_Model/BPR.py
@@ -21,7 +21,7 @@ def load_all(test_num=100):
     # 훈련 데이터 로드: user와 item 컬럼만 사용
     train_data = pd.read_csv(
         path + 'train.csv', 
-        sep=',', header=None, names=['user', 'item'], 
+        sep='\t', header=None, names=['user', 'item'], 
         usecols=[0, 1], dtype={0: np.int32, 1: np.int32})
 
     # 최대 user ID와 item ID를 계산하여 전체 사용자 수와 아이템 수를 파악
@@ -40,18 +40,15 @@ def load_all(test_num=100):
     test_data = []
     with open(path + 'test_neg_100.csv', 'r') as fd:
         line = fd.readline()
-        # 각 라인마다 사용자 ID와 해당 사용자에 대한 긍정적인 아이템 샘플과 부정적인 아이템 샘플을 추출
         while line != None and line != '':
             arr = line.split('\t')
             u = eval(arr[0])[0]  # 사용자 ID
             test_data.append([u, eval(arr[0])[1]])  # 긍정적 아이템 샘플
-            # 각 사용자에 대해 여러 부정적 아이템을 추가
             for i in arr[1:]:
                 test_data.append([u, int(i)])  # 부정적 아이템 샘플 추가
             line = fd.readline()
 
     return train_data, test_data, user_num, item_num, train_mat
-
 
 # BPR 데이터셋 클래스: PyTorch의 Dataset을 상속받아 사용자 정의 데이터셋 생성
 class BPRData(data.Dataset):
@@ -67,31 +64,27 @@ class BPRData(data.Dataset):
     def ng_sample(self):
         assert self.is_training, 'no need to sampling when testing'
         self.features_fill = []
-        # 각 샘플마다 부정적인 아이템을 샘플링하여 추가
         for x in self.features:
             u, i = x[0], x[1]
             for t in range(self.num_ng):
                 j = np.random.randint(self.num_item)
-                # 부정적인 아이템이 훈련 데이터에 없는 경우에만 추가
                 while (u, j) in self.train_mat:
                     j = np.random.randint(self.num_item)
                 self.features_fill.append([u, i, j])  # 사용자, 긍정적 아이템, 부정적 아이템
 
     # 데이터셋 크기 반환
     def __len__(self):
-        # 훈련 모드인 경우에는 부정적 샘플링을 고려하여 데이터셋 크기를 조정
         return self.num_ng * len(self.features) if self.is_training else len(self.features)
 
     # 데이터셋 요소 접근 함수
     def __getitem__(self, idx):
         features = self.features_fill if self.is_training else self.features
-        # 훈련 모드인 경우에는 부정적 아이템도 반환
         user = features[idx][0]
         item_i = features[idx][1]
         item_j = features[idx][2] if self.is_training else features[idx][1]
         return user, item_i, item_j  # 사용자, 긍정적 아이템, 부정적 아이템 반환
-
-
+      
+      
 # BPR 모델 정의 클래스: PyTorch의 Module을 상속받아 모델 정의
 class BPR(nn.Module):
     def __init__(self, user_num, item_num, factor_num):
@@ -105,110 +98,147 @@ class BPR(nn.Module):
 
     # 순전파 함수
     def forward(self, user, item_i, item_j):
-        user = self.embed_user(user)  # 사용자 임베딩
-        item_i = self.embed_item(item_i)  # 긍정적인 아이템 임베딩
-        item_j = self.embed_item(item_j)  # 부정적인 아이템 임베딩
-        # 사용자와 각 아이템의 내적을 계산하여 예측을 수행
+        user = self.embed_user(user)
+        item_i = self.embed_item(item_i)
+        item_j = self.embed_item(item_j)
         prediction_i = (user * item_i).sum(dim=-1)  # 긍정적 예측
         prediction_j = (user * item_j).sum(dim=-1)  # 부정적 예측
-        return prediction_i, prediction_j  # 긍정적 예측, 부정적 예측 반환
+        return prediction_i, prediction_j
 
+def hit(gt_item, pred_itemes):
+	if gt_item in pred_itemes:
+		return 1
+	return 0
+
+
+def ndcg(gt_item, pred_itemes):
+	if gt_item in pred_itemes:
+		index = pred_itemes.index(gt_item)
+		return np.reciprocal(np.log2(index+2))
+	return 0
+
+
+def metrics(model, test_loader, top_k):
+	HR, NDCG = [], []
+
+	for user, item_i, item_j in test_loader:
+		user = user.cuda()
+		item_i = item_i.cuda()
+		item_j = item_j.cuda() # not useful when testing
+
+		prediction_i, prediction_j = model(user, item_i, item_j)
+		_, indices = torch.topk(prediction_i, top_k)
+		recommends = torch.take(
+				item_i, indices).cpu().numpy().tolist()
+
+		gt_item = item_i[0].item()
+		HR.append(hit(gt_item, recommends))
+		NDCG.append(ndcg(gt_item, recommends))
+
+	return np.mean(HR), np.mean(NDCG)
 
 if __name__ == '__main__':
-    # argparse를 사용하여 스크립트 실행 시에 사용자가 설정할 수 있는 하이퍼파라미터 정의
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--lr", 
-        type=float, 
-        default=0.01, 
-        help="learning rate")  # 학습률
-    parser.add_argument("--lamda", 
-        type=float, 
-        default=0.001, 
-        help="model regularization rate")  # 모델 정규화 비율
-    parser.add_argument("--batch_size", 
-        type=int, 
-        default=4096, 
-        help="batch size for training")  # 훈련 배치 크기
-    parser.add_argument("--epochs", 
-        type=int,
-        default=50,  
-        help="training epoches")  # 훈련 에포크 수
-    parser.add_argument("--top_k", 
-        type=int, 
-        default=10, 
-        help="compute metrics@top_k")  # 메트릭 계산에 사용할 상위 K 항목
-    parser.add_argument("--factor_num", 
-        type=int,
-        default=32, 
-        help="predictive factors numbers in the model")  # 모델에서 사용할 예측 요소 수
-    parser.add_argument("--num_ng", 
-        type=int,
-        default=4, 
-        help="sample negative itemes for training")  # 훈련을 위한 부정적 항목 샘플링 수
-    parser.add_argument("--test_num_ng", 
-        type=int,
-        default=99, 
-        help="sample part of negative itemes for testing")  # 테스트를 위한 부정적 항목 샘플링 수
-    parser.add_argument("--out", 
-        default=True,
-        help="save model or not")  # 모델 저장 여부
-    parser.add_argument("--gpu", 
-        type=str,
-        default="0",  
-        help="gpu card ID")  # 사용할 GPU 카드 ID
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--lr", 
+		type=float, 
+		default=0.01, 
+		help="learning rate")
+	parser.add_argument("--lamda", 
+		type=float, 
+		default=0.001, 
+		help="model regularization rate")
+	parser.add_argument("--batch_size", 
+		type=int, 
+		default=4096, 
+		help="batch size for training")
+	parser.add_argument("--epochs", 
+		type=int,
+		default=50,  
+		help="training epoches")
+	parser.add_argument("--top_k", 
+		type=int, 
+		default=10, 
+		help="compute metrics@top_k")
+	parser.add_argument("--factor_num", 
+		type=int,
+		default=32, 
+		help="predictive factors numbers in the model")
+	parser.add_argument("--num_ng", 
+		type=int,
+		default=4, 
+		help="sample negative itemes for training")
+	parser.add_argument("--test_num_ng", 
+		type=int,
+		default=99, 
+		help="sample part of negative itemes for testing")
+	parser.add_argument("--out", 
+		default=True,
+		help="save model or not")
+	parser.add_argument("--gpu", 
+		type=str,
+		default="0",  
+		help="gpu card ID")
+	args = parser.parse_args()
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu  # 사용할 GPU 디바이스 설정
-    cudnn.benchmark = True  # cudnn을 사용하여 최적화된 학습을 위한 설정
+	os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+	cudnn.benchmark = True
 
-    # 데이터셋 준비
-    train_data, test_data, user_num ,item_num, train_mat = load_all()
 
-    # 훈련 및 테스트 데이터셋 구성
-    train_dataset = BPRData(train_data, item_num, train_mat, args.num_ng, True)
-    test_dataset = BPRData(test_data, item_num, train_mat, 0, False)
-    train_loader = data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
-    test_loader = data.DataLoader(test_dataset, batch_size=args.test_num_ng+1, shuffle=False, num_workers=0)
+	############################## PREPARE DATASET ##########################
+	train_data, test_data, user_num ,item_num, train_mat = load_all()
 
-    # 모델 생성
-    model = BPR(user_num, item_num, args.factor_num)
-    model.cuda()  # GPU로 모델 이동
+	# construct the train and test datasets
+	train_dataset = BPRData(
+			train_data, item_num, train_mat, args.num_ng, True)
+	test_dataset = BPRData(
+			test_data, item_num, train_mat, 0, False)
+	train_loader = data.DataLoader(train_dataset,
+			batch_size=args.batch_size, shuffle=True, num_workers=4)
+	test_loader = data.DataLoader(test_dataset,
+			batch_size=args.test_num_ng+1, shuffle=False, num_workers=0)
 
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.lamda)  # 옵티마이저 설정
+	########################### CREATE MODEL #################################
+	model = BPR(user_num, item_num, args.factor_num)
+	model.cuda()
 
-    # 학습 루프
-    count, best_hr = 0, 0
-    for epoch in range(args.epochs):
-        model.train()  # 모델을 학습 모드로 설정
-        start_time = time.time()
-        train_loader.dataset.ng_sample()  # 부정적 샘플링 수행
+	optimizer = optim.SGD(
+				model.parameters(), lr=args.lr, weight_decay=args.lamda)
+	# writer = SummaryWriter() # for visualization
 
-        for user, item_i, item_j in train_loader:
-            user = user.cuda()
-            item_i = item_i.cuda()
-            item_j = item_j.cuda()
+	########################### TRAINING #####################################
+	count, best_hr = 0, 0
+	for epoch in range(args.epochs):
+		model.train() 
+		start_time = time.time()
+		train_loader.dataset.ng_sample()
 
-            model.zero_grad()
-            prediction_i, prediction_j = model(user, item_i, item_j)  # 모델의 순전파 진행
-            loss = - (prediction_i - prediction_j).sigmoid().log().sum()  # BPR 손실 계산
-            loss.backward()  # 역전파 및 그래디언트 업데이트
-            optimizer.step()
-            count += 1  # 반복 횟수 증가
+		for user, item_i, item_j in train_loader:
+			user = user.cuda()
+			item_i = item_i.cuda()
+			item_j = item_j.cuda()
 
-        model.eval()  # 모델을 평가 모드로 설정
-        HR, NDCG = metrics(model, test_loader, args.top_k)  # 성능 메트릭 계산
+			model.zero_grad()
+			prediction_i, prediction_j = model(user, item_i, item_j)
+			loss = - (prediction_i - prediction_j).sigmoid().log().sum()
+			loss.backward()
+			optimizer.step()
+			# writer.add_scalar('data\\loss', loss.item(), count)
+			count += 1
 
-        elapsed_time = time.time() - start_time  # 경과 시간 계산
-        print("The time elapse of epoch {:03d}".format(epoch) + " is: " + 
-                time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
-        print("HR: {:.3f}\tNDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
+		model.eval()
+		HR, NDCG = metrics(model, test_loader, args.top_k)
 
-        # if HR > best_hr:
-        #     best_hr, best_ndcg, best_epoch = HR, NDCG, epoch
-        #     if args.out:
-        #         if not os.path.exists(path + model_path):
-        #             os.mkdir(path + model_path)
-        #         torch.save(model, '{}BPR.pt'.format(path + model_path))
+		elapsed_time = time.time() - start_time
+		print("The time elapse of epoch {:03d}".format(epoch) + " is: " + 
+				time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
+		print("HR: {:.3f}\tNDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
 
-    # print("End. Best epoch {:03d}: HR = {:.3f}, \
-    #     NDCG = {:.3f}".format(best_epoch, best_hr, best_ndcg))
+		# if HR > best_hr:
+		# 	best_hr, best_ndcg, best_epoch = HR, NDCG, epoch
+		# 	if args.out:
+		# 		if not os.path.exists(path + model_path):
+		# 			os.mkdir(path + model_path)
+		# 		torch.save(model, '{}BPR.pt'.format(path + model_path))
+
+	# print("End. Best epoch {:03d}: HR = {:.3f}, \
+	# 	NDCG = {:.3f}".format(best_epoch, best_hr, best_ndcg))
