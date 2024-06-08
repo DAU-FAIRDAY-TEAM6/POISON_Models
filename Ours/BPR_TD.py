77d7ff70e7ecfc25b689d3475c25c71807d0309e
choibumku00 <101037541+choibumku00@users.noreply.github.com>
2024-06-08 18:07:17 +0900

    TextDistBPR, alpha 추가

--- Ours/BPR_TD.py
+++ Ours/BPR_TD.py
@@ -3,9 +3,9 @@ from scipy.spatial.distance import pdist, squareform
 
 class TextDistBPR(nn.Module):
     '''
-    MF 모델에 대한 BPR 학습 
+    MF 모델에 대한 BPR 학습
     '''
-    def __init__(self, dataset, latent_factors, text_factors, learning_rate, reg):
+    def __init__(self, dataset, factors, text_factors, learning_rate, reg, init_mean, init_stdev, alpha):
         '''
         생성자
         Args:
@@ -16,64 +16,36 @@ class TextDistBPR(nn.Module):
             init_mean (float): 초기화에 사용되는 정규 분포의 평균.
             init_stdev (float): 초기화에 사용되는 정규 분포의 표준 편차.
         '''
-        super(TextDistBPR, self).__init__()
-        
+        super(TextBPR, self).__init__()
         self.dataset = dataset
         self.train_data = dataset.train
         self.test_data = dataset.test
-        self.test_for_eval = dataset.test_for_eval
         self.norm_distances = dataset.norm_distances
-        
+        # self.test_for_eval = dataset.test_for_eval
         self.num_user = dataset.num_user
         self.num_item = dataset.num_item
-        
-        self.user_review_embeds = dataset.user_review_embeds
-        self.poi_review_embeds = dataset.poi_review_embeds
         self.neg = dataset.neg
-        self.factors_MF = latent_factors
+        self.factors = factors
         self.factors_Text = text_factors
         self.learning_rate = learning_rate
         self.reg = reg
+        self.init_mean = init_mean
+        self.init_stdev = init_stdev
+        self.alpha = alpha
 
+        self.user_review_embeds = dataset.user_review_embeds.to(DEVICE)
+        self.poi_review_embeds = dataset.poi_review_embeds.to(DEVICE)
 
         # 사용자와 아이템의 잠재 요인을 초기화합니다.
-        #self.embed_user = torch.normal(mean=self.init_mean * torch.ones(self.num_user, self.factors_MF), std=self.init_stdev).requires_grad_()
-        #self.embed_item = torch.normal(mean=self.init_mean * torch.ones(self.num_item, self.factors_MF), std=self.init_stdev).requires_grad_()
-        
-        #self.embed_user_Text = torch.normal(mean=self.init_mean * torch.ones(self.num_user, self.factors_Text), std=self.init_stdev).requires_grad_()
-        
-        self.embed_user = nn.Embedding(self.num_user, self.factors_MF).to(DEVICE)
-        self.embed_item = nn.Embedding(self.num_item, self.factors_MF).to(DEVICE)
-        
-        self.embed_user_Text = nn.Embedding(self.num_user, self.factors_Text).to(DEVICE)
-        
-        self.E_u = nn.Embedding(768, self.factors_Text).to(DEVICE)
-        self.E1 = nn.Embedding(768, self.factors_Text).to(DEVICE)
-        self.E2 = nn.Linear(768, self.factors_Text, bias=False).to(DEVICE)
-        
-        self.beta_items = nn.Embedding(self.num_item, 1).to(DEVICE)
-        self.text_bias = nn.Embedding(768, 1).to(DEVICE)
-        
-        self._init_weight_()
-        
+        self.embed_user = torch.normal(mean=self.init_mean * torch.ones(self.num_user, self.factors), std=self.init_stdev).to(DEVICE).requires_grad_()
+        self.embed_item = torch.normal(mean=self.init_mean * torch.ones(self.num_item, self.factors), std=self.init_stdev).to(DEVICE).requires_grad_()
+
+        self.beta_items = torch.normal(mean=self.init_mean * torch.ones(self.num_item, 1), std=self.init_stdev).to(DEVICE).requires_grad_()
+        self.text_bias = torch.normal(mean=self.init_mean * torch.ones(768, 1), std=self.init_stdev).to(DEVICE).requires_grad_()
+
         # Adam optimizer를 초기화합니다.
-        self.mf_optim = optim.Adam(self.parameters(), lr=self.learning_rate, weight_decay= self.reg)
+        self.mf_optim = optim.Adam([self.embed_user, self.embed_item, self.beta_items, self.text_bias], lr=self.learning_rate, weight_decay=1e-5)
 
-    def _init_weight_(self):
-        # weight 초기화, 표준편차 : 0.01
-        nn.init.xavier_uniform_(self.embed_user.weight)
-        nn.init.xavier_uniform_(self.embed_item.weight)
-
-        # Visual factors (theta)
-        nn.init.xavier_uniform_(self.embed_user_Text.weight)
-        nn.init.xavier_uniform_(self.E1.weight)
-        nn.init.xavier_uniform_(self.E2.weight)
-        nn.init.xavier_uniform_(self.E_u.weight)
-
-        # Biases (beta)
-        nn.init.xavier_uniform_(self.beta_items.weight)
-        nn.init.xavier_uniform_(self.text_bias.weight)
-    
     def forward(self, u, i, j):
         '''
         MF-BPR 모델의 forward pass입니다.
@@ -86,48 +58,44 @@ class TextDistBPR(nn.Module):
             y_uj: 사용자와 부정적인 아이템 간의 예측 점수.
             loss: BPR 손실.
         '''
-        user_latent_factor = self.embed_user(u) # batch * latent
-        #user_text_factors = self.embed_user_Text(u) # batch * latent
-        user_text_factors = self.user_review_embeds[u] #/ math.sqrt(786)
-        
-        i_bias = self.beta_items(i) # batch * 1
-        j_bias = self.beta_items(j) # batch * 1
-        
-        i_latent_factors = self.embed_item(i) # batch * latent
-        j_latent_factors = self.embed_item(j) # batch * latent
-        
+        # 사용자와 긍정적인 아이템 간의 예측 점수 계산
+
+        user_latent_factor = self.embed_user[u]
+        user_text_factors = self.user_review_embeds[u] / math.sqrt(786)
+        alpha = self.alpha
+
+
+        i_bias = self.beta_items[i] # batch * 1
+        j_bias = self.beta_items[j] # batch * 1
+
         i_text_factors = self.poi_review_embeds[i] # batch * 768
         j_text_factors = self.poi_review_embeds[j] # batch * 768
-        
-        
-        diff_latent_factors = i_latent_factors - j_latent_factors # batch * latent
-        diff_text_factors = (i_text_factors - j_text_factors) #/ math.sqrt(768) # batch * 768
 
-        #latent_matrix = (user_latent_factor * diff_latent_factors).sum(dim=-1).unsqueeze(-1) 
-        #text_matrix = (user_text_factors.mm(self.E_u.weight) * diff_text_factors.mm(self.E1.weight)).sum(dim=-1).unsqueeze(-1)
-        #text_matrix = (user_text_factors.mm(self.E_u.weight) * self.E2(diff_text_factors)).sum(dim=-1).unsqueeze(-1)
-        
+        i_latent_factors = self.embed_item[i]
+        j_latent_factors = self.embed_item[j]
+
+        diff_latent_factors = i_latent_factors - j_latent_factors # batch * latent
+        diff_text_factors = (i_text_factors - j_text_factors) / math.sqrt(768) # batch * 768
+
         if diff_text_factors.shape[0] == 768: # [768], eval set이라면
             user_latent_factor = user_latent_factor.unsqueeze(0)
             user_text_factors = user_text_factors.unsqueeze(0)
             diff_text_factors = diff_text_factors.unsqueeze(0) # [1, text_emb]
             diff_latent_factors = diff_latent_factors.unsqueeze(0) # [ 1, latent_emb]
-            
-        text_bias = diff_text_factors.mm(self.text_bias.weight)
-        #user_factors = torch.cat((user_latent_factor, user_text_factors), dim=1)
-        #item_factors = torch.cat((diff_latent_factors, diff_text_factors), dim=1)
-        user_factors = torch.cat((user_latent_factor, user_text_factors.mm(self.E_u.weight)), dim=1)
-        item_factors = torch.cat((diff_latent_factors, diff_text_factors.mm(self.E1.weight)), dim=1)
-        u_i_score = (user_factors * item_factors).sum(dim=-1).unsqueeze(-1)
-        
-        # i와 j 정규화 거리
-        distance_ij = torch.tensor(self.norm_distances[i, j]).to(DEVICE)
-        
-        #x_uij = i_bias - j_bias + latent_matrix + text_matrix + text_bias
+
+        latent_factor = (user_latent_factor * diff_latent_factors).sum(dim=-1).unsqueeze(-1)
+        text_factor = (user_text_factors * diff_text_factors).sum(dim=-1).unsqueeze(-1)
+
+        u_i_score = alpha * latent_factor + (1 - alpha) * text_factor
+        text_bias = diff_text_factors.mm(self.text_bias)
+
         x_uij = i_bias - j_bias + u_i_score + text_bias
 
-        x_uij = distance_ij * x_uij
+        distance_ij = torch.tensor(self.norm_distances[i, j]).to(DEVICE)
         
+        x_uij = distance_ij * x_uij
+        # 정규화 항 계산
+        # BPR 손실 계산
         loss = -torch.sum(torch.log(torch.sigmoid(x_uij.unsqueeze(0))))
         return loss
 
@@ -141,95 +109,94 @@ class TextDistBPR(nn.Module):
         '''
         data_loader = DataLoader(self.dataset, batch_size=batch_size)
 
-        print("Training Text-BPR with: learning_rate=%.4f, regularization=%.7f, factors=%d, #epoch=%d, batch_size=%d."
-               % (self.learning_rate, self.reg, self.factors_MF, epoch, batch_size))
+        print("Training MF-BPR with: learning_rate=%.4f, regularization=%.7f, factors=%d, #epoch=%d, batch_size=%d."
+               % (self.learning_rate, self.reg, self.factors, epoch, batch_size))
         t1 = time.time()
-        
+
         max_hit, max_precision, max_recall, max_recall_epoch, max_precision_epoch, max_hit_epoch = 0,0,0,0,0,0
         for epoc in range(epoch):
-            self.train()
             iter_loss = 0
             count = 0
-            for s, (users, items_pos, items_negs) in enumerate(data_loader):
+            for s, (users, items_pos, items_neg) in enumerate(data_loader):
+                users = users.to(DEVICE)
+                items_pos = items_pos.to(DEVICE)
+                items_neg = items_neg.to(DEVICE)
+
                 count += 1
                 # 기울기 초기화
                 self.mf_optim.zero_grad()
                 # Forward pass를 통해 예측과 손실 계산
-                for items_neg in items_negs:
-                    users = users.to(DEVICE)
-                    items_pos = items_pos.to(DEVICE)
-                    items_neg = items_neg.to(DEVICE)
-                    
-                    loss = self.forward(users, items_pos, items_neg)
-                    iter_loss += loss.item()
+                loss = self.forward(users, items_pos, items_neg)
+                iter_loss += loss
                 # Backward pass 및 파라미터 업데이트
                 loss.backward()
                 self.mf_optim.step()
             t2 = time.time()
-            
+
             # 성능 측정 함수를 통해 HitRatio 및 NDCG를 계산
-            if epoc % 5 == 0:
-                self.eval()
-                hits, recall, precision = self.evaluate_model(self.test_data, topK)
-                eval_loss = 0
-                for idx, (u, i, j) in enumerate(self.test_for_eval):
-                    loss = self.forward(u, i, j)
-                    eval_loss += loss.item()
-                total_samples = len(self.test_for_eval)
-                eval_loss = eval_loss / total_samples if total_samples > 0 else 0
-                iter_loss = iter_loss / count / batch_size
-                print(f"epoch={epoc}, train_loss = {iter_loss:.6}, test_loss = {eval_loss:.6}[{t2-t1:.2}s] HitRatio@{topK} = {hits:.6}, RECAll@{topK} = {recall:.6}, PRECISION@{topK} = {precision:.6} [{time.time()-t2:.1}s]")
-                if precision > max_precision:
-                    max_precision = precision
-                    max_precision_epoch = epoc
-                if recall > max_recall:
-                    max_recall = recall
-                    max_recall_epoch = epoc
-                if hits > max_hit:
-                    max_hit = hits
-                    max_hit_epoch = epoc
-                t1 = time.time()
-        save_perform(reg, batch_size, latent_factors, text_factors, epoc, learning_rate, max_hit, max_hit_epoch, max_recall, max_recall_epoch, max_precision, max_precision_epoch)
-                
-                
+            hits, recall, precision = self.evaluate_model(self.test_data, topK)
+            # eval_loss = 0
+            # for idx, (u, i, j) in enumerate(self.test_for_eval):
+            #     u, i, j = u.to(DEVICE), i.to(DEVICE), j.to(DEVICE)
+            #     loss = self.forward(u, i, j)
+            #     eval_loss += loss
+            # total_samples = len(self.test_for_eval)
+            # eval_loss = eval_loss / total_samples if total_samples > 0 else 0
+            # iter_loss = iter_loss / count / batch_size
+            print(f"epoch={epoc}, train_loss = {iter_loss:.6} [{int(t2-t1)}s] HitRatio@{topK} = {hits:.6}, RECAll@{topK} = {recall:.6}, PRECISION@{topK} = {precision:.6} [{int(time.time()-t2)}s], alpha: {alpha}")
+            t1 = time.time()
+            if precision > max_precision:
+                max_precision = precision
+                max_precision_epoch = epoc
+            if recall > max_recall:
+                max_recall = recall
+                max_recall_epoch = epoc
+            if hits > max_hit:
+                max_hit = hits
+                max_hit_epoch = epoc
+            t1 = time.time()
+
+        #save_perform(reg, batch_size, latent_factors, text_factors, epoc, learning_rate, max_hit, max_hit_epoch, max_recall, max_recall_epoch, max_precision, max_precision_epoch, alpha)
+
+
     def evaluate_model(self, test, K):
         """
         Top-K 추천의 성능(Hit_Ratio, NDCG)을 평가합니다.
         반환값: 각 테스트 상호작용의 점수.
         """
-        with torch.no_grad():
-            user_latent_factor = self.embed_user.weight # batch * latent
-            item_latent_factors = self.embed_item.weight # batch * latent
-            
-            #user_text_factors = self.embed_user_Text.weight # batch * latent
-            user_text_factors = self.user_review_embeds #/ math.sqrt(768) # batch * latent
-            item_text_factors = self.poi_review_embeds #/ math.sqrt(768)# batch * 768
-            
-            #score_matrix = (torch.mm(user_latent_factor, item_latent_factors.t()) + torch.mm(user_text_factors.mm(self.E_u.weight), item_text_factors.mm(self.E1.weight).t()))
-            #score_matrix = (torch.mm(user_latent_factor, item_latent_factors.t()) + torch.mm(user_text_factors.mm(self.E_u.weight), self.E2(item_text_factors).t()))
-            #score_matrix = (torch.mm(user_latent_factor, item_latent_factors.t()) + torch.mm(user_text_factors, item_text_factors.t()))
-            
-            #user_factors = torch.cat((user_latent_factor, user_text_factors), dim=1)
-            #item_factors = torch.cat((item_latent_factors, item_text_factors), dim=1)
-            user_factors = torch.cat((user_latent_factor, user_text_factors.mm(self.E_u.weight)), dim=1)
-            item_factors = torch.cat((item_latent_factors, item_text_factors.mm(self.E1.weight)), dim=1)
-            score_matrix = torch.mm(user_factors, item_factors.t())
-            ########
-            top_scores, top_indicies = torch.topk(score_matrix, K, dim=1)
-            
-            hit = 0
-            sum_recall = 0
-            sum_precision = 0
-            for u,hist in enumerate(test):
-                set_topk = set(i.item() for i in (top_indicies[u]))
-                set_hist = set(hist)
-                
-                if set_hist & set_topk:
-                    hit += 1
-                sum_precision += len(set_hist & set_topk) / len(set_topk)
-                sum_recall += len(set_hist & set_topk) / len(set_hist)
-                
-        return hit / len(test), sum_recall / len(test), sum_precision / len(test)
+        user_latent_factor = self.embed_user # batch * latent
+        item_latent_factors = self.embed_item # batch * latent
+
+        user_text_factors = self.user_review_embeds / math.sqrt(768) # batch * latent
+        item_text_factors = self.poi_review_embeds / math.sqrt(768)# batch * 768
+
+
+        latent_score_matrix = torch.mm(user_latent_factor, item_latent_factors.t())
+        text_score_matrix = torch.mm(user_text_factors, item_text_factors.t())
+
+        score_matrix = self.alpha * latent_score_matrix + (1-self.alpha) * text_score_matrix
+
+        item_bias = self.beta_items.squeeze()
+        item_bias = item_bias.view(1, -1)
+        score_matrix = score_matrix + item_bias
+
+
+        top_scores, top_indicies = torch.topk(score_matrix, K, dim=1)
+
+        hits = 0
+        sum_recall = 0
+        sum_precision = 0
+        for u,hist in enumerate(test):
+            set_topk = set(i.item() for i in (top_indicies[u]))
+            set_hist = set(hist)
+
+            if set_hist & set_topk:
+                hits += 1
+            sum_precision += len(set_hist & set_topk) / len(set_topk)
+            sum_recall += len(set_hist & set_topk) / len(set_hist)
+
+        return hits / len(test), sum_recall / len(test), sum_precision / len(test)
+
 
 
 class Yelp(Dataset):
